283ed839b9a44893e103a4cb313c8fc6838241b1
M Hakim Saputra <a1011201296@student.untan.ac.id>
2025-03-09 17:39:58 +0700

    feat(movies): update API endpoint for popular movies and add movie list styles

--- pages/movies/movies.js
+++ pages/movies/movies.js
@@ -56,7 +56,7 @@ const createData = async () => {
     // const url ="https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc";
     // const url = "https://jsonplaceholder.typicode.com/users";
     const url =
-      "https://api.themoviedb.org/3/movie/popular?language=en-US&page=7";
+      "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1";
     const response = await fetch(url, objOption);
     if (!response.ok) {
       throw new Error(response.statusText);
